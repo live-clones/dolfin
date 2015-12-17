5fcf5fe0fe72286acc86a5510fd7062ae560a261
Chris Richardson <chris@bpi.cam.ac.uk>
2015-12-17 22:10:34 +0000

    Tidy formatting

--- dolfin/geometry/Plane.cpp
+++ dolfin/geometry/Plane.cpp
@@ -15,7 +15,6 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
 
-
 #include <cmath>
 #include <dolfin/common/constants.h>
 #include <dolfin/log/log.h>
@@ -34,46 +33,49 @@ using namespace dolfin;
 /// the plane d.
 Plane::Plane(Point n, double d)
 {
-  if (n.norm() == (0.0))
-    {dolfin_error("Plane.cpp",
+  if (n.norm() == 0.0)
+  {
+    dolfin_error("Plane.cpp",
                  "constructing a plane using a normal vector and a distance",
                  "invalid normal vector supplied");
-    }
+  }
   _n = n/n.norm();
   _d = d;
 }
 //-----------------------------------------------------------------------------
 /// Constructs a plane using a normal vector n and a point on the plane
-    /// a.
+/// a.
 Plane::Plane(Point a, Point n)
 {
-  if (n.norm() == (0.0))
-    {dolfin_error("Plane.cpp",
+  if (n.norm() == 0.0)
+  {
+    dolfin_error("Plane.cpp",
                  "constructing a plane using a normal vector and a distance",
                  "invalid normal vector supplied");
-    }
+  }
   _n = n/n.norm();
   _d = a.dot(_n);
 }
 //-----------------------------------------------------------------------------
-    /// Constructs a plane using three points on the plane.
+/// Constructs a plane using three points on the plane.
 Plane::Plane(Point a, Point b, Point c)
 {
-  Point n = (b-a).cross(c-a);
-  if (n.norm() == (0.0))
-    {dolfin_error("Plane.cpp",
+  Point n = (b - a).cross(c - a);
+  if (n.norm() == 0.0)
+  {
+    dolfin_error("Plane.cpp",
                  "constructing a plane using a normal vector and a distance",
                  "invalid normal vector supplied");
-    }
+  }
   _n = n/n.norm();
   _d = a.dot(_n);
 }
 //-----------------------------------------------------------------------------
 /// Checks to see if an edge intersects a plane and returns T/F and the
 /// intersection point.  If no intersection returns (0, 0, 0).
-std::pair<bool, Point> Plane::intersection( const Edge& e ) const
+std::pair<bool, Point> Plane::intersection(const Edge& e) const
 {
-  std::pair<bool, Point>  result = {false, Point(0.0, 0.0, 0.0)};
+  std::pair<bool, Point> result = {false, Point(0.0, 0.0, 0.0)};
 
   const Mesh& mesh = e.mesh();
   Point p0 = Vertex(mesh, e.entities(0)[0]).point();
@@ -84,13 +86,14 @@ std::pair<bool, Point> Plane::intersection( const Edge& e ) const
   {
     result.first = true;
     double diff = p0.dot(_n) - p1.dot(_n);
-    if (diff == (0.0))
-      {dolfin_error("Plane.cpp",
-                 "checking to see if edge and plane intersect",
-                 "divide by zero error.  p0.n = p1.n");
-      }
+    if (diff == 0.0)
+    {
+      dolfin_error("Plane.cpp",
+                   "checking to see if edge and plane intersect",
+                   "divide by zero error.  p0.n = p1.n");
+    }
     double lambda = (_d - p1.dot(_n))/(p0.dot(_n) - p1.dot(_n));
-    result.second = lambda*p0 + (1-lambda)*p1;
+    result.second = lambda*p0 + (1.0 - lambda)*p1;
   }
   else
     result.first = false;
