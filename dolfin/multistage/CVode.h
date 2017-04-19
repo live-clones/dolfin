7d774df074bda58cb02fb845710d4f0b18218f6d
Chris Richardson <chris@bpi.cam.ac.uk>
2017-04-19 15:50:17 +0100

    Update CVode class

--- dolfin/multistage/CVode.h
+++ dolfin/multistage/CVode.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2007 Garth N. Wells
+// Copyright (C) 2017 Chris Richardson and Chris Hadjigeorgiou
 //
 // This file is part of DOLFIN.
 //
@@ -14,21 +14,12 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
-//
-// Modified by Chris Hadjigeorgiou: 2017.
-//
-// First added:  2017-01-31
-// Last changed: 2011-02-07
 
 #ifndef __DOLFIN_C_VODE_H
 #define __DOLFIN_C_VODE_H
 
 #ifdef HAS_SUNDIALS
 
-#include <string>
-#include <utility>
-#include <memory>
-#include <dolfin/common/types.h>
 #include <cvode/cvode.h>
 #include <dolfin/la/SUNDIALSNVector.h>
 
@@ -41,49 +32,53 @@ namespace dolfin
   {
   public:
 
-    /// Create empty CVode object 
-    CVode(MPI_Comm comm=MPI_COMM_WORLD)
+    /// Constructor
+    CVODE()
     {
-      C_V = CVodeCreate(CV_BDF,CV_NEWTON);
+      // Create CVODE memory block
+      cvode_mem = CVodeCreate(CV_BDF, CV_NEWTON);
+      dolfin_assert(cvode_mem);
+
+      // Point user_data back to this object
+      // (for use in "f" below)
+      int flag = CVodeSetUserData(cvode_mem, (void *)this);
+      dolfin_assert(flag == 0);
     }
 
-    /// Create CVode object with size N
-    CVode(MPI_Comm comm, int lmm, int iter)
-    {
-      C_V = CVodeCreate(lmm, iter);
-    }
-
-    /// Destructor 
+    /// Destructor
     ~CVode()
     {
-      CVodeFree(&C_V);
+      PrintFinalStats(cvode_mem);  // Print some final statistics (debug use)
+      CVodeFree(&cvode_mem);
     }
 
-    //--- Implementation of CVode functions
-    void init(std::shared_ptr<dolfin::SUNDIALSNVector> u0, CVRhsFn f, double t, double atol, double rtol)
-    {
-      CVodeInit(C_V,f,t,u0->nvector());
-      CVodeSStolerances(C_V,atol, rtol);
-    }
+    /// Initialise CVode
+    void init(std::shared_ptr<GenericVector> u0, double atol, double rtol);
 
-    double step(std::shared_ptr<dolfin::SUNDIALSNVector> u0, double dt, double *t)
-    {
-      ::CVode(C_V, dt, u0->nvector(), t, CV_NORMAL);
-    }
+    /// Advance time by timestep dt
+    double step(double dt);
 
-    //-----------------------------------------------------------------------------
+    /// Get current time
+    double time()
+    { return t; }
 
-    /// Assignment operator
-    const CVode& operator= (const CVode& x)
-    { return *this; }
-    
+    /// Overloaded function for time derivatives of u at time t
+    /// Given the vector u, at time t, provide the time derivative udot.
+    virtual void derivs(double t, std::shared_ptr<GenericVector> u,
+                        std::shared_ptr<GenericVector> udot);
 
   private:
+    // Internal callback from CVode to get time derivatives - passed on to derivs (above)
+    static int f(realtype t, N_Vector u, N_Vector udot, void *user_data);
+
+    // Vector of values - wrapper around dolfin::GenericVector
+    std::shared_ptr<SUNDIALSNVector> _u;
+
+    // Current time
+    double t;
 
     // Pointer to CVode memory struct
-    void * C_V;
-    void * cv_mem()
-    {  return C_V; }
+    void *cvode_mem;
 
   };
 
