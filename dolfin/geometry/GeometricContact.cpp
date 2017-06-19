0948a1ac17283aaa383007e913ed99806ba94468
Chris Richardson <chris@bpi.cam.ac.uk>
2017-06-19 15:17:42 +0100

    Some fixes and remove accidentally added stuff from other branch

--- dolfin/geometry/GeometricContact.cpp
+++ dolfin/geometry/GeometricContact.cpp
@@ -16,8 +16,10 @@
 // along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
 //
 
+#include <dolfin/common/ArrayView.h>
 #include <dolfin/function/Function.h>
 #include <dolfin/mesh/Mesh.h>
+#include <dolfin/mesh/MeshEditor.h>
 #include <dolfin/mesh/Facet.h>
 #include <dolfin/mesh/Vertex.h>
 #include "Point.h"
@@ -34,34 +36,33 @@ std::vector<Point> GeometricContact::create_deformed_segment_volume_3d(Mesh& mes
   Point X1 = Vertex(mesh, facet.entities(0)[0]).point();
   Point X2 = Vertex(mesh, facet.entities(0)[1]).point();
   Point X3 = Vertex(mesh, facet.entities(0)[2]).point();
-  Point x1 = X1 + Point(u(X1));
-  Point x2 = X2 + Point(u(X2));
-  Point x3 = X3 + Point(u(X3));
 
+  Array<double> uval(3);
 
-  // Cast out prism defined by three tetrahedra from the master facet to its displaced configuration
-  return {X1, X2, X3, x1,
-          X2, X3, x1, x2,
-          X3, x1, x2, x3};
+  const Array<double> _x1(3, X1.coordinates());
+  u.eval(uval, _x1);
+  Point x1 = X1 + Point(uval);
+
+  const Array<double> _x2(3, X2.coordinates());
+  u.eval(uval, _x2);
+  Point x2 = X2 + Point(uval);
+
+  const Array<double> _x3(3, X3.coordinates());
+  u.eval(uval, _x3);
+  Point x3 = X3 + Point(uval);
+
+  return {X1, X2, X3, x1, x2, x3};
 }
 //-----------------------------------------------------------------------------
-bool GeometricContact::check_tet_set_collision(const std::vector<Point>& tet_set1, const std::vector<Point>& tet_set2)
+bool GeometricContact::check_tet_set_collision(const Mesh& master_mesh, std::size_t mindex,
+                                               const Mesh& slave_mesh, std::size_t sindex)
 {
-  dolfin_assert(tet_set1.size() == tet_set2.size());
-  dolfin_assert(tet_set1.size() %4 == 0);
-  const std::size_t ntets = tet_set1.size() / 4;
 
-  for (unsigned int i = 0; i < ntets; ++i)
-    for (unsigned int j = 0; i < ntets; ++i)
+  for (unsigned int i = mindex; i < mindex + 3; ++i)
+    for (unsigned int j = sindex; j < sindex + 3; ++j)
     {
-      if (CollisionDetection::collides_tetrahedron_tetrahedron(tet_set1[i*4],
-                                                               tet_set1[i*4+1],
-                                                               tet_set1[i*4+2],
-                                                               tet_set1[i*4+3],
-                                                               tet_set2[j*4],
-                                                               tet_set2[j*4+1],
-                                                               tet_set2[j*4+2],
-                                                               tet_set2[j*4+3]))
+      if (CollisionDetection::collides_tetrahedron_tetrahedron(Cell(master_mesh, i),
+                                                               Cell(slave_mesh, j)))
         return true;
     }
 
@@ -70,7 +71,7 @@ bool GeometricContact::check_tet_set_collision(const std::vector<Point>& tet_set
 //-----------------------------------------------------------------------------
 std::map<std::size_t, std::vector<std::size_t>>
   GeometricContact::contact_surface_map_volume_sweep_3d(Mesh& mesh, Function& u,
-  const std::vector<std::size_t>& master_facets, const std::vector<std::size_t>& slave_facets)
+                                                        const std::vector<std::size_t>& master_facets, const std::vector<std::size_t>& slave_facets)
 {
   // Construct a dictionary mapping master facets to their collided slave counterparts.
 
@@ -85,16 +86,70 @@ std::map<std::size_t, std::vector<std::size_t>>
 
   std::map<std::size_t, std::vector<std::size_t>> master_to_slave;
 
+  // Local mesh of master 'prisms', three tetrahedra created per facet
+  Mesh master_mesh(MPI_COMM_SELF);
+  MeshEditor master_ed;
+  master_ed.open(master_mesh, mesh.topology().dim(), mesh.geometry().dim());
+  master_ed.init_vertices(master_facets.size()*6);
+  master_ed.init_cells(master_facets.size()*3);
+  int c = 0;
+  int v = 0;
+
+  std::cout << "mf.size() = " << master_facets.size() << "\n";
+
   for (auto &mf : master_facets)
   {
+    std::cout << "c = " << c << "\n";
+
     std::vector<Point> master_tet_set = create_deformed_segment_volume_3d(mesh, mf, u);
-    for (auto &sf : slave_facets)
+    for (int i = 0; i < 3; ++i)
+      master_ed.add_cell(c+i, v+i, v+i+1, v+i+2, v+i+3);
+    c += 3;
+    for (int i = 0; i < 6; ++i)
+      master_ed.add_vertex(v+i, master_tet_set[i]);
+    v += 6;
+  }
+  master_ed.close();
+
+  // Local mesh of slave 'prisms', three tetrahedra created per facet
+  Mesh slave_mesh(MPI_COMM_SELF);
+  MeshEditor slave_ed;
+  slave_ed.open(slave_mesh, mesh.topology().dim(), mesh.geometry().dim());
+  slave_ed.init_vertices(slave_facets.size()*6);
+  slave_ed.init_cells(slave_facets.size()*3);
+  c = 0;
+  v = 0;
+  for (auto &sf : slave_facets)
+  {
+    std::vector<Point> slave_tet_set = create_deformed_segment_volume_3d(mesh, sf, u);
+    for (int i = 0; i < 3; ++i)
+      slave_ed.add_cell(c+i, v+i, v+i+1, v+i+2, v+i+3);
+    c += 3;
+    for (int i = 0; i < 6; ++i)
+      slave_ed.add_vertex(v+i, slave_tet_set[i]);
+    v += 6;
+  }
+  slave_ed.close();
+
+  // Check each master 'prism' against each slave 'prism'
+  for (unsigned int i = 0; i < master_facets.size(); ++i)
+    for (unsigned int j = 0; j < slave_facets.size(); ++j)
     {
-      std::vector<Point> slave_tet_set = create_deformed_segment_volume_3d(mesh, sf, u);
-      if (check_tet_set_collision(master_tet_set, slave_tet_set))
+      std::size_t mf = master_facets[i];
+      std::size_t sf = slave_facets[j];
+
+      if (check_tet_set_collision(master_mesh, i*3, slave_mesh, j*3))
         master_to_slave[mf].push_back(sf);
     }
+
+
+  for (auto m : master_to_slave)
+  {
+    std::cout << m.first << ":";
+    for (auto q : m.second)
+      std::cout << q << " ";
+    std::cout << "\n";
   }
 
-  return master_to_slave;
+   return master_to_slave;
 }
