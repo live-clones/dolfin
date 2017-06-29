e6287c44672b77986ab3d8ccbb43c7912102722a
Garth N. Wells <gnw20@cam.ac.uk>
2017-06-29 19:36:48 +0100

    Re-enable some mesh wrapping.

--- python/dolfin/generation/generation.cpp
+++ python/dolfin/generation/generation.cpp
@@ -33,14 +33,14 @@ namespace dolfin_wrappers
   void generation(py::module& m)
   {
     // Wrap dolfin::UnitSquareMesh
-    //py::class_<dolfin::UnitSquareMesh, std::shared_ptr<dolfin::UnitSquareMesh>, dolfin::Mesh>(m, "UnitSquareMesh")
-    //  .def(py::init<std::size_t, std::size_t>())
-    //  .def(py::init<std::size_t, std::size_t, std::string>());
+    py::class_<dolfin::UnitSquareMesh, std::shared_ptr<dolfin::UnitSquareMesh>, dolfin::Mesh>(m, "UnitSquareMesh")
+      .def(py::init<std::size_t, std::size_t>())
+      .def(py::init<std::size_t, std::size_t, std::string>());
 
     // Wrap dolfin::UnitCubeMesh
-    //py::class_<dolfin::UnitCubeMesh, std::shared_ptr<dolfin::UnitCubeMesh>, dolfin::Mesh>(m, "UnitCubeMesh")
-    //  .def(py::init<std::size_t, std::size_t, std::size_t>())
-    //  .def(py::init<MPI_Comm, std::size_t, std::size_t, std::size_t>());
+    py::class_<dolfin::UnitCubeMesh, std::shared_ptr<dolfin::UnitCubeMesh>, dolfin::Mesh>(m, "UnitCubeMesh")
+      .def(py::init<std::size_t, std::size_t, std::size_t>())
+      .def(py::init<MPI_Comm, std::size_t, std::size_t, std::size_t>());
 
     // Wrap dolfin::BoxMesh
     py::class_<dolfin::BoxMesh, std::shared_ptr<dolfin::BoxMesh>, dolfin::Mesh>(m, "BoxMesh")
