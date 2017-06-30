e2d2187cb77d42b691b873435167b72b98312a71
Garth N. Wells <gnw20@cam.ac.uk>
2017-06-30 20:17:21 +0100

    Add UFL statements to pybind11 test.

--- python/tests/function_space.py
+++ python/tests/function_space.py
@@ -2,7 +2,19 @@ import ufl
 import dolfin_test.function.functionspace
 import dolfin_test.cpp.generation
 
+# Create mesh
 mesh = dolfin_test.cpp.generation.UnitSquareMesh(6, 9)
 
-#V = dolfin_test.cpp.function.FunctionSpace()
+# Create function space
 V = dolfin_test.function.functionspace.FunctionSpace(mesh, "Lagrange", 1)
+
+# Variation problem
+u = ufl.TrialFunction(V)
+v = ufl.TestFunction(V)
+
+a = ufl.dot(ufl.grad(u), ufl.grad(v))*(ufl.dx)
+L = 1.0*v*(ufl.dx)
+
+# Assemble
+
+# TODO
