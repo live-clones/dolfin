0a5070f14dc2e1ed84d9e6d5502ab20047f661ba
Garth N. Wells <gnw20@cam.ac.uk>
2017-07-10 08:24:20 +0100

    Clean-up __init__.py

--- python/dolfin/__init__.py
+++ python/dolfin/__init__.py
@@ -26,24 +26,30 @@ del sys
 #del sys
 
 # cpp modules
-from .cpp.common import Variable, has_debug, has_hdf5, has_hdf5_parallel, \
-    has_mpi, has_petsc, has_slepc, git_commit_hash, DOLFIN_EPS, DOLFIN_PI
+from .cpp.common import (Variable, has_debug, has_hdf5,
+                         has_hdf5_parallel, has_mpi, has_petsc,
+                         has_slepc, git_commit_hash, DOLFIN_EPS,
+                         DOLFIN_PI)
 from .cpp import MPI
 from .cpp.function import Expression, Constant, Function, interpolate
 from .cpp.fem import FiniteElement, DofMap, Assembler, assemble
 from .cpp.geometry import BoundingBoxTree, Point, MeshPointIntersection, intersect
-from .cpp.generation import IntervalMesh, UnitIntervalMesh, \
-    UnitSquareMesh, UnitCubeMesh, BoxMesh, \
-    RectangleMesh, UnitQuadMesh
+from .cpp.generation import (IntervalMesh, UnitIntervalMesh,
+                             UnitSquareMesh, UnitCubeMesh, BoxMesh, RectangleMesh,
+                             UnitQuadMesh)
 from .cpp.io import XDMFFile
-from .cpp.la import has_linear_algebra_backend, linear_algebra_backends, has_krylov_solver_method, \
-    has_krylov_solver_preconditioner
+from .cpp.la import (has_linear_algebra_backend,
+                     linear_algebra_backends, has_krylov_solver_method,
+                     has_krylov_solver_preconditioner)
+
 if has_linear_algebra_backend('PETSc'):
     from .cpp.la import PETScVector, PETScMatrix
-from .cpp.la import Matrix, Vector, EigenMatrix, EigenVector, LUSolver, KrylovSolver
-from .cpp.mesh import Mesh, MeshTopology, MeshGeometry, MeshEntity, Cell, Facet, \
-    Face, Edge, Vertex, cells, facets, faces, edges, vertices, SubDomain, BoundaryMesh, \
-    MeshEditor
+
+from .cpp.la import (Matrix, Vector, EigenMatrix, EigenVector,
+                     LUSolver, KrylovSolver)
+from .cpp.mesh import (Mesh, MeshTopology, MeshGeometry, MeshEntity,
+                       Cell, Facet, Face, Edge, Vertex, cells, facets, faces, edges,
+                       vertices, SubDomain, BoundaryMesh, MeshEditor)
 from .cpp.parameter import parameters
 from .cpp.refinement import refine
 
@@ -53,11 +59,12 @@ from .fem.dirichletbc import DirichletBC, CompiledSubDomain
 from .function.functionspace import FunctionSpace
 from .function.constant import Constant
 from .function.expression import CompiledExpression, UserExpression
-from .mesh.meshfunction import MeshFunction, CellFunction, FacetFunction, FaceFunction, \
-    EdgeFunction, VertexFunction
+from .mesh.meshfunction import (MeshFunction, CellFunction,
+                                FacetFunction, FaceFunction, EdgeFunction, VertexFunction)
 
 # ufl
-from ufl import TestFunction, TrialFunction, inner, dot, grad, dx, ds, dS, triangle, tetrahedron
+from ufl import (TestFunction, TrialFunction, inner, dot, grad, dx,
+                 ds, dS, triangle, tetrahedron)
 
 # FIXME
 def has_petsc4py():
