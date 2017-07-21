8285fc3b1a7281128351bdbbf90bd6698db0290e
Cécile Daversin-Catty <cecile@simula.no>
2017-07-21 15:56:09 +0200

    Add demos solving mixed(-dimensional) problems [based on FunctionSpaceProduct, MixedLinearVariational(Problem/Solver)]

---
+++ demo/undocumented/block-assembly-2D2D/python/demo_block-assembly-2D2D.py
@@ -0,0 +1,59 @@
+from dolfin import *
+
+#Create mesh and define function space
+mesh = UnitSquareMesh(32, 32)
+
+marker = CellFunction("size_t", mesh, 0)
+for c in cells(mesh):
+    marker[c] = c.midpoint().x() < 0.5
+
+submesh1 = MeshViewMapping.create_from_marker(marker, 1)
+submesh2 = MeshViewMapping.create_from_marker(marker, 0)
+
+# Define Dirichlet boundary
+def boundarySub1(x):
+    return x[0] < DOLFIN_EPS
+
+def boundarySub2(x):
+    return x[0] > 1.0 - DOLFIN_EPS
+
+#element2D = FiniteElement("Lagrange", triangle, 1)
+W1 = FunctionSpace(submesh1, "Lagrange", 1)
+W2 = FunctionSpace(submesh2, "Lagrange", 2)
+
+# Define the product function space
+V = FunctionSpaceProduct( W1, W2 )
+
+# Define boundary conditions
+u0 = Constant(0.0)
+# Subdomain 1
+bc1 = DirichletBC(V.sub_space(0), u0, boundarySub1)
+# Subdomain 2
+bc2 = DirichletBC(V.sub_space(1), u0, boundarySub2)
+bcs = [bc1,bc2]
+
+# Define variational problem
+# Use directly TrialFunction and TestFunction on the product space
+(u1,u2) = TrialFunction(V)
+(v1,v2) = TestFunction(V)
+
+f = Expression("10*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)", degree=2)
+
+# Global weak form
+a = inner(grad(u1), grad(v1))*dx + inner(grad(u2), grad(v2))*dx
+L = f*v1*dx + f*v2*dx
+
+# Solve the problem
+sol = Function(V)
+solver = PETScKrylovSolver("minres")
+solve(a == L, sol, bcs)
+
+# extract components of the solution
+sol1 = sol.sub(0)
+sol2 = sol.sub(1)
+
+## Save solution in vtk format
+out_sub1 = File("block-assembly-2D2D-subdomain1.pvd")
+out_sub1 << sol1
+out_sub2 = File("block-assembly-2D2D-subdomain2.pvd")
+out_sub2 << sol2
---
+++ demo/undocumented/block-assembly-3D1D/python/demo_block-assembly-3D1D.py
@@ -0,0 +1,59 @@
+from dolfin import *
+
+# Create mesh and define function space
+mesh = UnitCubeMesh(32, 32, 32)
+
+marker = EdgeFunction("size_t", mesh, 0)
+for e in edges(mesh):
+    marker[e] = 0.5 - DOLFIN_EPS < e.midpoint().z() < 0.5 + DOLFIN_EPS and 0.5 - DOLFIN_EPS < e.midpoint().y() < 0.5 + DOLFIN_EPS
+
+submesh = MeshViewMapping.create_from_marker(marker, 1)
+
+W1 = FunctionSpace(mesh, "Lagrange", 1) ## 3D
+W2 = FunctionSpace(submesh, "Lagrange", 1) ## 1D
+
+# Define the product function space
+V = FunctionSpaceProduct( W1, W2 )
+
+# Define Dirichlet boundary (x = 0 or x = 1)
+def boundary(x):
+    return x[0] < DOLFIN_EPS or x[0] > 1.0 - DOLFIN_EPS
+
+# Define boundary condition
+u0 = Constant(0.0)
+# 3D mesh
+bc_3D = DirichletBC(V.sub_space(0), u0, boundary)
+# 1D submesh
+bc_1D = DirichletBC(V.sub_space(1), u0, boundary)
+bcs = [bc_3D,bc_1D]
+
+# Define variational problem
+# Use directly TrialFunction and TestFunction on the product space
+(u_3D,u_1D) = TrialFunction(V)
+(v_3D,v_1D) = TestFunction(V)
+
+f = Expression("10*exp(-(pow(x[0] - 0.5, 2) ) / 0.02)", degree=2)
+
+# Global weak form
+a = inner(grad(u_3D), grad(v_3D))*dx + inner(grad(u_1D), grad(v_1D))*dx
+L = f*v_3D*dx + f*v_1D*dx
+
+# Solve the problem
+sol = Function(V)
+rtol=1e-4
+print("******************************************************************")
+print("NOTE : Relative tolerance for Krylov solver has been set to", rtol)
+print("With default solver/preconditioner, convergence is very slow.")
+print("We need to find a better configuration.")
+print("******************************************************************")
+solve(a == L, sol, bcs=bcs, solver_parameters={"krylov_solver":{"relative_tolerance":rtol, "maximum_iterations":10000}})
+
+# extract components of the solution
+sol_3D = sol.sub(0)
+sol_1D = sol.sub(1)
+
+## Save solution in vtk format
+out_3D = File("block-assembly-3D1D-3Dsol.pvd")
+out_3D << sol_3D
+out_1D = File("block-assembly-3D1D-1Dsol.pvd")
+out_1D << sol_1D
