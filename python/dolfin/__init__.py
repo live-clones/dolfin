00d9b5afd4921904798731063748b6d1ba81bdd5
Chris Richardson <chris@bpi.cam.ac.uk>
2017-08-09 15:55:08 +0100

    Import split from ufl

--- python/dolfin/__init__.py
+++ python/dolfin/__init__.py
@@ -115,7 +115,7 @@ from .mesh.subdomain import CompiledSubDomain
 
 # ufl
 from ufl import (FiniteElement, VectorElement, MixedElement, rhs, lhs,
-                 conditional, le, lt, ge, gt,
+                 conditional, le, lt, ge, gt, split,
                  inner, dot, grad, dx, div, Measure, det, sin, cos, ln, exp, sqrt,
                  ds, dS, dP, triangle, tetrahedron, avg, jump, sym, tr, Identity)
 from ufl.formoperators import action
