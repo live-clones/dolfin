5e6c006fdabbcc0fa53d9b7199e6ba9dc022e2e5
Chris Richardson <chris@bpi.cam.ac.uk>
2017-08-15 12:23:40 +0100

    Fix up BlockVector

--- python/dolfin/__init__.py
+++ python/dolfin/__init__.py
@@ -65,7 +65,7 @@ if has_slepc():
 
 from .cpp.la import (IndexMap, DefaultFactory, Matrix, Vector, Scalar, EigenMatrix,
                      EigenVector, EigenFactory, LUSolver, KrylovSolver, TensorLayout,
-                     LinearOperator)
+                     LinearOperator, BlockMatrix, BlockVector)
 from .cpp.log import info, Table, set_log_level
 from .cpp.math import ipow, near, between
 from .cpp.mesh import (Mesh, MeshTopology, MeshGeometry, MeshEntity, MeshColoring,
