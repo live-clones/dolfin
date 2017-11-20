c7e06a907c05bf809a3624c9b2df8b58b9d8b726
Chris Hadjigeorgiou <Chrishas@gmail.com>
2017-11-20 15:40:58 +0000

    Switch test to use Newton method

--- test/unit/python/multistage/test_sundials.py
+++ test/unit/python/multistage/test_sundials.py
@@ -7,18 +7,21 @@ class MyCVode(CVode):
     # Overload the derivs method
     def derivs(self, t, u, udot):
         udot[:] = -u[:]
-
+    def Jacobian(self, u, udot, t, y, fy):
+        return 0
+    def psolve(self, t, u, udot, r, z, gamma, x, y):
+        return 0
 
 def test_sundials():
 
     if has_sundials():
       phi = Vector(mpi_comm_world(),20)
       phi[:] = 1.0
-      cv = MyCVode(CVode.LMM.CV_ADAMS,CVode.ITER.CV_FUNCTIONAL)
+      cv = MyCVode(CVode.LMM.CV_BDF,CVode.ITER.CV_NEWTON)
       cv.init(phi, 1e-6, 1e-6)
 
       nstep = 20
-      dt = 0.1
+      dt = 0.001
       for i in range(nstep):
         t = cv.step(dt)       
-        assert (exp(-t)-phi[i])<1e-5
+        assert (exp(-t)-phi[i])<1e-3
