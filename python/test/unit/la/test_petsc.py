6150725ae808b55519f46558c4c34a901d2a306c
Benjamin Kehlet <benjamik@simula.no>
2017-12-17 01:07:12 +0100

    Guard import of PETSc types in an "if has_petsc()" to avoid crash if dolfin is configured without petsc.

--- python/test/unit/la/test_petsc.py
+++ python/test/unit/la/test_petsc.py
@@ -21,10 +21,14 @@ GenericFoo interface
 # along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import print_function
-from dolfin import (PETScVector, PETScMatrix, PETScLUSolver,
-                    PETScKrylovSolver, UnitSquareMesh, TrialFunction,
-                    TestFunction, mpi_comm_self, mpi_comm_world,
-                    FunctionSpace, assemble, Constant, dx, parameters)
+from dolfin import (UnitSquareMesh, TrialFunction, TestFunction,
+                    mpi_comm_self, mpi_comm_world, FunctionSpace,
+                    assemble, Constant, dx, parameters, has_petsc)
+
+if has_petsc() :
+    from dolfin import (PETScVector, PETScMatrix,
+                        PETScLUSolver, PETScKrylovSolver)
+
 from dolfin_utils.test import (skip_if_not_PETSc,
                                skip_if_not_petsc4py,
                                pushpop_parameters)
