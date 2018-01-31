2a8630bc99bfc7ee97f33a3ba879511c9af1a705
Philipp Trunschke <trunschk@math.hu-berlin.de>
2018-01-31 11:48:43 +0000

    allow float(constant) if constant.ufl_shape == (1,)
    constant.py edited online with Bitbucket

--- python/dolfin/function/constant.py
+++ python/dolfin/function/constant.py
@@ -114,9 +114,9 @@ class Constant(ufl.Coefficient):
 
     def __float__(self):
         # Overriding UFL operator in this particular case.
-        if self.ufl_shape:
-            raise TypeError("Cannot convert nonscalar constant to float.")
-        return float(self._cpp_object)
+        if self.ufl_shape == (): return float(self._cpp_object)
+        elif self.ufl_shape == (1,): return float(self.values()[0])
+        else: raise TypeError("Cannot convert nonscalar constant to float.")
 
     def str(self, verbose):
         return self._cpp_object.str(verbose)
