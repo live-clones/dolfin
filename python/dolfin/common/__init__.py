b8ca9772b6fe1dbac4ead917f2443826f314ae9e
jorgensd <dokken92@gmail.com>
2018-02-15 15:20:43 +0100

    Fixed unecessary complexity in Progess

--- python/dolfin/common/__init__.py
+++ python/dolfin/common/__init__.py
@@ -1,20 +1,10 @@
 import dolfin.cpp as cpp
 
 
-def _add(self, incr):
+def __iadd__(self, incr):
     self._increment(incr)
-
-
-def _set(self, value):
-    self._assign(value)
-
-
-def __iadd__(self, other):
-    """ Add value to Progress """
-    self._add(other)
     return self
 
 
-cpp.log.Progress._add = _add
 cpp.log.Progress.__iadd__ = __iadd__
-del __iadd__, _add
+del __iadd__
