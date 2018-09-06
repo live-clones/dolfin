a355dc4ffdba7b71fa8abbd2cab341cfc84dd2b7
nate-sime <nsime@carnegiescience.edu>
2018-09-06 18:50:42 -0400

    provide option not to VecCreate each SLEPcEigenSolver::get_eigenpair call

--- python/src/la.cpp
+++ python/src/la.cpp
@@ -1074,6 +1074,12 @@ namespace dolfin_wrappers
              dolfin::PETScVector r, c;
              self.get_eigenpair(lr, lc, r, c, i);
              return py::make_tuple(lr, lc, r, c);
+           })
+      .def("get_eigenpair", [](dolfin::SLEPcEigenSolver& self, dolfin::PETScVector& r, dolfin::PETScVector& c, std::size_t i)
+           {
+             double lr, lc;
+             self.get_eigenpair(lr, lc, r, c, i);
+             return py::make_tuple(lr, lc);
            });
     #endif
 
